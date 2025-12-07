9cc6a34b474216388f446ab0bed19020459ebca0
majusilva25 <mariajulia.silva@estudante.ufjf.br>
2025-12-07 17:15:11 -0300

    adicionando perguntas

--- src/componentes/FiltroPele/FiltroPele.tsx
+++ src/componentes/FiltroPele/FiltroPele.tsx
@@ -3,9 +3,18 @@ export default function FiltroPele(){
 cconst [etapa, setEtapa] = useState(1);
   const [respostas, setRespostas] = useState({});
   const [resultado, setResultado] = useState(null);
-}
+
 
 function salvarResposta(pergunta, valor) {
     setRespostas(prev => ({ ...prev, [pergunta]: valor }));
     setEtapa(etapa + 1);
   }
+
+function perguntas{
+    return(
+if (etapa === 5){
+    console.log("Teste finalizado!");
+} 
+    )
+}
+}
