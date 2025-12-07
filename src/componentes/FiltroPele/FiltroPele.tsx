1a6d9161214f0122e89daf48c87337354f603953
majusilva25 <mariajulia.silva@estudante.ufjf.br>
2025-12-07 17:03:48 -0300

    funcao para ir salvando as respostas

--- src/componentes/FiltroPele/FiltroPele.tsx
+++ src/componentes/FiltroPele/FiltroPele.tsx
@@ -4,3 +4,8 @@ cconst [etapa, setEtapa] = useState(1);
   const [respostas, setRespostas] = useState({});
   const [resultado, setResultado] = useState(null);
 }
+
+function salvarResposta(pergunta, valor) {
+    setRespostas(prev => ({ ...prev, [pergunta]: valor }));
+    setEtapa(etapa + 1);
+  }
