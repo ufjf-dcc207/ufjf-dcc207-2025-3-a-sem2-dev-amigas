ebb18888a6f47aaa9a8563990f31346e3b27a39f
majusilva25 <mariajulia.silva@estudante.ufjf.br>
2025-12-08 12:35:36 +0800

    colocando sinal check para opcao selecionada

--- src/componentes/FiltroPele/FiltroPele.tsx
+++ src/componentes/FiltroPele/FiltroPele.tsx
@@ -1,3 +1,4 @@
+import { tipo } from "react-icons/ti";
 
 
 export default function FiltroPele() {
@@ -12,149 +13,134 @@ export default function FiltroPele() {
     }
 
 
-    const opcoes=[baixa, media, alta];
+    const opcoes = ["baixa", "media", "alta"];
 
-//titulo é o titulo da pergunta e nome-opcoes é o que será passado p  salvarResposta, ex:oleosidade:baixa
-    function perguntas(titulo, nome, opcoes){
+    //titulo é o titulo da pergunta e nome-opcoes é o que será passado p  salvarResposta, ex:oleosidade:baixa
+    function perguntas(titulo, nome, opcoes) {
 
 return(
+        <div className="box">
+            <h3>{titulo}</h3>
 
-         <div className="box">
-                <h3>{titulo}</h3>
 
-
-        {opcoes.map(op => (
+            {opcoes.map(op => (
             <div
         key={op}
         className={"item " + (respostas[nome] === op ? "ativo" : "")}
-        onClick={() => selecionar(nome, op)}
-    ></div>
-        )
+        onClick={() => salvarResposta(nome, op)}
+    >
 
-
-
-        )
-
-
-        if (etapa === 5) {
-            return (
-        <div>
-          <button onClick={finaliza}>Finalizar análise de pele</button>
-        </div>
-      );
-        }
-
-
-        return (
-            <div>
-                {
-                    etapa === 1 && (
-                        <div>
-                            <h2>Quanto sua pele produz de oleosidade?</h2>
-                            <button onClick={() => salvarResposta("oleosidade", "baixa")}>Baixa</button>
-                            <button onClick={() => salvarResposta("oleosidade", "media")}>Média</button>
-                            <button onClick={() => salvarResposta("oleosidade", "alta")}>Alta</button>
-                        </div>
-
-                    )
-                }
-
-                {
-                    etapa === 2 && (
-                        <div>
-                            <h2>Como sua pele reage a produtos novos?</h2>
-                            <button onClick={() => salvarResposta("sensibilidade", "baixa")}>Quase não reage</button>
-                            <button onClick={() => salvarResposta("sensibilidade", "media")}>Às vezes irrita</button>
-                            <button onClick={() => salvarResposta("sensibilidade", "alta")}>Fica vermelha/sensível</button>
-                        </div>
-                    )
-                }
-
-                {
-                    etapa === 3 && (
-                        <div>
-
-                            <h2>Com que frequência aparecem espinhas em você?</h2>
-                            <button onClick={() => salvarResposta("acne", "baixa")}>Raramente</button>
-                            <button onClick={() => salvarResposta("acne", "media")}>Algumas vezes</button>
-                            <button onClick={() => salvarResposta("acne", "alta")}>Com frequência</button>
-                        </div>
-                    )
-                }
-
-                {etapa === 4 && (
-                    <div>
-                        <h2>Você sente sua pele repuxando após lavar o rosto?</h2>
-                        <button onClick={() => salvarResposta("ressecamento", "nao")}>Não</button>
-                        <button onClick={() => salvarResposta("ressecamento", "leve")}>Um pouco</button>
-                        <button onClick={() => salvarResposta("ressecamento", "sim")}>Sim, repuxa</button>
-                    </div>
-                )
-                }
-
-                {etapa === 5 && (
-                    <div>
-                        <h2>Você sente brilho excessivo na zona T (testa, nariz e queixo)?</h2>
-                        <button onClick={() => salvarResposta("zonaT", "nao")}>Não</button>
-                        <button onClick={() => salvarResposta("zonaT", "leve")}>Um pouco</button>
-                        <button onClick={() => salvarResposta("zonaT", "sim")}>Sim, bastante</button>
-                    </div>
-                )
-                }
-</div>
-                
-
-                )
-            }
-        }
-
-
-
-
-
-        function finaliza(){
-            let pontos = {
-  oleosa: 0,
-  seca: 0,
-  mista: 0,
-  normal: 0,
-  sensivel:0
-};
-
-
-if (respostas.oleosidade === "alta") pontos.oleosa++;
-if (respostas.oleosidade === "baixa") pontos.seca++;
-if (respostas.oleosidade === "media") pontos.normal++;
-
-if (respostas.sensibilidade === "alta") pontos.seca++;
-if (respostas.sensibilidade === "baixa") pontos.normal++;
-if (respostas.sensibilidade === "media") pontos.mista++;
-
-if (respostas.zonaT === "sim") pontos.mista++;
-if (respostas.zonaT === "leve") pontos.normal++;
-if (respostas.zonaT === "nao") pontos.seca++;
-
-if (respostas.ressecamento === "sim") pontos.seca++;
-  if (respostas.ressecamento === "leve") pontos.normal++;
-  if (respostas.ressecamento === "nao") pontos.oleosa++;
-
-  if (respostas.acne === "alta") pontos.oleosa++;
-  if (respostas.acne === "media") pontos.mista++;
-  if (respostas.acne === "baixa") pontos.normal++;
-
-
-
-
-  let tipoFinal = "oleosa"; 
-
-  if (pontos.seca > pontos[tipoFinal]) tipoFinal = "seca";
-  if (pontos.mista > pontos[tipoFinal]) tipoFinal = "mista";
-  if (pontos.normal > pontos[tipoFinal]) tipoFinal = "normal";
-  if (pontos.sensivel > pontos[tipoFinal]) tipoFinal = "sensivel";
-
-
-  setResultado(tipoFinal);
-   setEtapa(0);
-        setRespostas({});
-        }
+        <span className="check">
+            {respostas[nome] === op && "✓"}
+            </span>
+            <span>{op}</span>
+            </div> 
+        ))
     }
+</div>
+    );
+}
+
+
+if(etapa === 1) return perguntas(
+        "Quanto sua pele produz de oleosidade?",
+        "oleosidade",
+        ["baixa","media","alta"]
+    );
+
+    if(etapa === 2) return perguntas(
+        "Como sua pele reage a produtos novos?",
+        "sensibilidade",
+        ["baixa","media","alta"]
+    );
+
+    if(etapa === 3) return perguntas(
+        "Com que frequência aparecem espinhas?",
+        "acne",
+        ["baixa","media","alta"]
+    );
+
+    if(etapa === 4) return perguntas(
+        "Sua pele repuxa após lavar?",
+        "ressecamento",
+        ["nao","leve","sim"]
+    );
+
+
+    if(etapa === 5){
+        return (
+            <div className="box">
+
+                {perguntas(
+                    "Brilho na Zona T?",
+                    "zonaT",
+                    ["nao","leve","sim"]
+                )}
+
+                <button onClick={finaliza}>
+                    Finalizar análise
+                </button>
+
+            </div>
+        );
+    }
+
+
+
+
+
+function finaliza() {
+    let pontos = {
+        oleosa: 0,
+        seca: 0,
+        mista: 0,
+        normal: 0,
+        sensivel: 0
+    };
+
+
+    if (respostas.oleosidade === "alta") pontos.oleosa++;
+    if (respostas.oleosidade === "baixa") pontos.seca++;
+    if (respostas.oleosidade === "media") pontos.normal++;
+
+    if (respostas.sensibilidade === "alta") pontos.seca++;
+    if (respostas.sensibilidade === "baixa") pontos.normal++;
+    if (respostas.sensibilidade === "media") pontos.mista++;
+
+    if (respostas.zonaT === "sim") pontos.mista++;
+    if (respostas.zonaT === "leve") pontos.normal++;
+    if (respostas.zonaT === "nao") pontos.seca++;
+
+    if (respostas.ressecamento === "sim") pontos.seca++;
+    if (respostas.ressecamento === "leve") pontos.normal++;
+    if (respostas.ressecamento === "nao") pontos.oleosa++;
+
+    if (respostas.acne === "alta") pontos.oleosa++;
+    if (respostas.acne === "media") pontos.mista++;
+    if (respostas.acne === "baixa") pontos.normal++;
+
+
+
+
+    let tipo = "oleosa";
+
+    if (pontos.seca > pontos[tipo]) tipo = "seca";
+    if (pontos.mista > pontos[tipo]) tipo = "mista";
+    if (pontos.normal > pontos[tipo]) tipo = "normal";
+    if (pontos.sensivel > pontos[tipo]) tipo = "sensivel";
+
+
+    setResultado(tipo);
+    setEtapa(0);
+    setRespostas({});
+}
+
+ if(resultado){
+        return (
+            <div className="box">
+                <h2>Seu tipo de pele é: {resultado}</h2>
+            </div>
+        );
+    }
+    
+}
